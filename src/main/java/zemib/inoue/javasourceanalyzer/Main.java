f5b4a5395b29f7cfd89bdb6a3f5448f5707c3713
hkr-kubo <67777772+hkr-kubo@users.noreply.github.com>
2022-01-10 21:21:19 +0900

    main関数に解析データをJSONで出力する処理を記述

--- src/main/java/zemib/inoue/javasourceanalyzer/Main.java
+++ src/main/java/zemib/inoue/javasourceanalyzer/Main.java
@@ -12,18 +12,18 @@ import com.github.javaparser.ast.CompilationUnit;
 public class Main {
 	public static void main(String[] args) {
 		Path source = Paths.get("src/main/java/zemib/inoue/javasourceanalyzer/Main.java");
-
+		ParseData parsedata = new ParseData();
+		Package packagedata = new Package();
 		try {
 			JavaParser parser = new JavaParser();
 			ParseResult<CompilationUnit> result = parser.parse(source);
 			ZemiBVoidVisitor zemibvoidvisitor = new ZemiBVoidVisitor();
 			result.getResult().ifPresent(r -> r.accept(zemibvoidvisitor, null));
 			ObjectMapper mapper = new ObjectMapper();
-			String json = mapper.writeValueAsString(zemibvoidvisitor.getClasses());
+			zemibvoidvisitor.getClasses().forEach(c -> packagedata.addClass(c));
+			parsedata.addPackage(packagedata);
+			String json = mapper.writeValueAsString(parsedata);
 			System.out.println(json);
-			for(int i=0; i<1; i++) {
-				
-			}
 		} catch (IOException e) {
 			e.printStackTrace(System.err);
 		}
